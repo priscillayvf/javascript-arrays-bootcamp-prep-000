922ee7f55bca328aaaa07cd63ba0edcfcb61b371
Priscilla Firpo <priscillayf@gmail.com>
2019-07-27 23:04:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -18,3 +18,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
 }
+function accessElementInArray(array, index) {
+  var array = [1, 2, 3]
+  return array[2]
+}
