2a4a46daed5d7e214f3fdb9107d75ed38b17c674
Priscilla Firpo <priscillayf@gmail.com>
2019-07-27 23:53:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,6 +24,6 @@ function accessElementInArray(array, index) {
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   var numb = [1, 2, 3];
-  numb.shift();
+  numb.shift(array);
   return numb;
 }
